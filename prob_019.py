9799decbdcf4d843e4effd562352c62daff04d47
Ryan W. Sinnet <ryan@rwsinnet.com>
2013-12-26 12:53:10 -0500

    Solved problem 19.

--- prob_019.py
+++ prob_019.py
@@ -1,15 +1,38 @@
 #!/usr/bin/python
-#unsolved
 
-no_leap = [31, 28, 31, 30, 31, 30,
-           31, 31, 30, 31, 30, 31]
-yes_leap = [31, 29, 31, 30, 31, 30,
-            31, 31, 30, 31, 30, 31]
+# Project Euler
+# Problem 19
+# Counting Sundays
+
+import numpy
+
+no_leap  = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+yes_leap = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+leaps = []
+
+day0 = 1 # monday
+days = 0
+first_month_days = []
 
 for i in range(1900, 2001):
-    if not i % 4:
-        yes_leap
-    else:
-        no_leap
 
-    range(1, 8):
+    if not i % 4 and (i % 100 or not i % 400):
+        leaps.append(i)
+        ld = yes_leap
+    else:
+        ld = no_leap
+
+    print i, sum(ld)
+    first_month_days.append([sum(ld[:k]) + days for k in range(len(ld)+1)])
+    days += sum(ld)
+
+z = numpy.array(first_month_days).flatten()
+print z[-1]+1
+
+y = set(z)
+w = set([x + 6 for x in range(0, z[-1]+1, 7)])
+q = [x for x in y.intersection(w) if x >= sum(no_leap)]
+
+#print [x - sum(no_leap) for x in q]
+print len(q)
