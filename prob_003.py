19515d9b579c06cc60e578aabb18160d92c0351d
Ryan W. Sinnet <ryan@rwsinnet.com>
2013-12-26 10:37:06 -0500

    Solved problem 3.

--- prob_003.py
+++ prob_003.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python
-#unsolved
-
-import math
 
 z = 600851475143
-n = int(math.sqrt(z))
+m = int(1e4)
+
 i = 2
 
-comp_nums = [False] * n
-while i <= n / 2:
+# Find all prime numbers up to ten thousand
+comp_nums = [False] * m
+while i <= m / 2:
     if not comp_nums[i-1]:
-        for x in range(2*i, n+1, i):
+        for x in range(2*i, m+1, i):
             comp_nums[x-1] = True
     i += 1
-print [j+1 for j, y in enumerate(comp_nums) if not y]
+foo = [j+1 for j, y in enumerate(comp_nums) if not y and j+1 > 1]
+
+print 'Found primes up to ten thousand! Factoring...'
+
+for x in foo:
+    if not z % x:
+        y = z / x
+        print '{} / {} = {}'.format(z, x, y)
+        z = y
+
+if z == 1:
+    print 'Factorized!'
