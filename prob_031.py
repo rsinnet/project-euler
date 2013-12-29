f44a39d0fafc6ca98dd297f8e93f4895845eb674
Ryan W. Sinnet <ryan@rwsinnet.com>
2013-12-29 14:29:24 -0500

    Solved problem 31.

--- prob_031.py
+++ prob_031.py
@@ -4,82 +4,16 @@
 # Problem 31
 # Coin sums
 
-# unsolved
-
 coins = [200, 100, 50, 20, 10, 5, 2, 1]
-
 i = 0
+M = 200
 
-for a1 in range(200 / coins[1] + 1):
-    bar = [0, a1, 0, 0, 0, 0, 0, 0]
-    baz =  a1 * coins[1]
-    if baz == 200:
-        i += 1
-        print i, bar, baz
-        continue
-    if baz > 200:
-        continue
-
-    for a2 in range(200 / coins[1]+1):
-        bar = [0, a1, a2, 0, 0, 0, 0, 0]
-        baz =  sum([bar[j] * coins[j] for j in range(1,3)])
-        if baz == 200:
-            i += 1
-            print i, bar, baz
-            continue
-        if baz > 200:
-            continue
-
-        for a3 in range(200 / coins[3]+1):
-            bar = [0, a1, a2, a3, 0, 0, 0, 0]
-            baz =  sum([bar[j] * coins[j] for j in range(1,4)])
-            if baz == 200:
-                i += 1
-                print i, bar, baz
-                continue
-            if baz > 200:
-                continue
-
-            for a4 in range(200 / coins[4] + 1):
-                bar = [0, a1, a2, a3, a4, 0, 0, 0]
-                baz =  sum([bar[j] * coins[j] for j in range(1,5)])
-                if baz == 200:
-                    i += 1
-                    print i, bar, baz
-                    continue
-                if baz > 200:
-                    continue
-
-                for a5 in range(200 / coins[5] + 1):
-                    bar = [0, a1, a2, a3, a4, a5, 0, 0]
-                    baz =  sum([bar[j] * coins[j] for j in range(1,6)])
-                    if baz == 200:
-                        i += 1
-                        print i, bar, baz
-                        continue
-                    if baz > 200:
-                        continue
-
-                    for a6 in range(200 / coins[6] + 1):
-                        bar = [0, a1, a2, a3, a4, a5, a6, 0]
-                        baz =  sum([bar[j] * coins[j] for j in range(1,7)])
-                        if baz == 200:
+for a0 in range(M, -1, -coins[0]):
+    for a1 in range(a0, -1, -coins[1]):
+        for a2 in range(a1, -1, -coins[2]):
+            for a3 in range(a2, -1, -coins[3]):
+                for a4 in range(a3, -1, -coins[4]):
+                    for a5 in range(a4, -1, -coins[5]):
+                        for a6 in range(a5, -1, -coins[6]):
                             i += 1
-                            print i, bar, baz
-                            continue
-                        if baz > 200:
-                            continue
-
-                        for a7 in range(200 / coins[7] + 1):
-                            bar = [0, a1, a2, a3, a4, a5, a6, a7]
-                            baz =  sum([bar[j] * coins[j] for j in range(1,8)])
-                            if baz == 200:
-                                i += 1
-                                print i, bar, baz
-                                continue
-                            if baz > 200:
-                                continue
-i += 1
-print i, [1, 0, 0, 0, 0, 0, 0, 0], 200
-
 print i
