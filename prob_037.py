4ef1b14c47b0a1224a67b6b536d7c4b0c488f727
Ryan W. Sinnet <ryan@rwsinnet.com>
2013-12-30 17:08:48 -0500

    Solved problem 37.

--- prob_037.py
+++ prob_037.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
-#unsolved
 
-import math
+# Project Euler
+# Problem 37
+# Truncatable primes
 
-n=1000000
+from math import log10
+
+n=int(1e6)
 i = 2
 
 comp_nums = [False] * n
@@ -13,6 +16,40 @@ while i <= n / 2:
             comp_nums[x-1] = True
     i += 1
 
-print len([j+1 for j, y in enumerate(comp_nums) if not y])
-foo = [j+1 for j, y in enumerate(comp_nums) if not y]
+foo = [j+1 for j, y in enumerate(comp_nums) if not y and j+1 > 1]
+#print foo
 
+comp_nums[0] = True
+
+
+def is_truncatable(x):
+    s = str(x)
+    l = int(log10(x)) + 1
+    #print 'length: {}'.format(l)
+    if comp_nums[x - 1]:
+        return False
+
+    for i in range(1, l):
+        #print s[i:]
+        if comp_nums[int(s[i:]) - 1]:
+            return False
+
+    for i in range(1, l):
+        #print s[:-i]
+        if comp_nums[int(s[:-i]) - 1]:
+            return False
+
+    return True
+
+t_primes = []
+
+for x in foo:
+    if x < 10:
+        continue
+
+    if is_truncatable(x):
+        t_primes.append(x)
+        print x
+print t_primes
+
+print 'Sum = {}.'.format(sum(t_primes))
